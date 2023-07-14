c16aae0ff6ad53341f9b2b266c7d29ef53aa57bc
lnajnb <138968368+lnajnb@users.noreply.github.com>
2023-07-14 13:08:54 +0700

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,22 @@
+#the colors
+white = (255, 255, 255)
+black = (0, 0, 0)
+dark_gray = (40, 40, 40)
+light_gray = (100, 100, 100)
+green = (6,214,160)
+blue = (0, 0, 255)
+red = (255, 0, 0)
+yellow = (255,209,102)
+bgcolor = dark_gray
+
+#the settings
+width = 600
+height = 800
+FPS=50
+title = "Wordle"
+tile_size = 80
+gap_size = 10
+
+#margins
+x_margin = int((width - (5 * (tile_size + gap_size))) // 2)
+y_margin = int((height - (6 * (tile_size + gap_size))) // 2)
